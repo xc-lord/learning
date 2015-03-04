f36738acb4de9c452c48b529910f2fdfef3f73da
unknown <miraclelord@163.com>
2015-03-04 16:13:28 +0800

    测试git

--- xc-learn/learn-ssh/src/main/java/com/lord/dao/ICatalogDao.java
+++ xc-learn/learn-ssh/src/main/java/com/lord/dao/ICatalogDao.java
@@ -6,10 +6,9 @@ import com.lord.model.Category;
 public interface ICatalogDao extends IGenericDao<Catalog, String> {
 
 	/**
-	 * 根据目录ID,查找对应根分类
+	 * 根据目录ID，查找对应根分类
 	 * @param id
 	 * @return
 	 */
 	Category getCategory(String id);
-
 }
